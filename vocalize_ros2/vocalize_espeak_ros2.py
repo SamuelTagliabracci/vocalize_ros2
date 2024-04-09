3ab60fd6692e646dc5c82758175a77274fe8c0b5
Samuel Tagliabracci <Sam@cornelltech.ca>
2024-04-09 02:01:42 +0000

    update

--- vocalize_ros2/vocalize_espeak_ros2.py
+++ vocalize_ros2/vocalize_espeak_ros2.py
@@ -12,6 +12,7 @@ import rclpy
 import subprocess
 from std_msgs.msg import String
 from rclpy.node import Node
+from rclpy.parameter import Parameter
 
 class VocalizeNode(Node):
     def __init__(self):
@@ -29,16 +30,33 @@ class VocalizeNode(Node):
             10
         )
 
-    voice = "zh-yue"
-    speed = 150
-    amplitude = 150
-    pitch = 90
-    wordgap = 15
-    lastdata = ""
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('voice', 'zh-yue'),
+                ('speed', 150),
+                ('amplitude', 150),
+                ('pitch', 90),
+                ('wordgap', 15)
+            ]
+        )
+
+        self.lastdata = ""
+        self.voice = ""
+        self.speed = 0
+        self.amplitude = 0
+        self.pitch = 0
+        self.wordgap = 0
+
+    def get_parameters(self):
+        self.voice = self.get_parameter('voice').value
+        self.speed = self.get_parameter('speed').value
+        self.amplitude = self.get_parameter('amplitude').value
+        self.pitch = self.get_parameter('pitch').value
+        self.wordgap = self.get_parameter('wordgap').value
 
     def response_callback(self, msg):
-    #    node = rclpy.get_node('vocalize_node')
-    #    node.get_logger().info('Vocalize: %s', msg.data)
+        self.get_parameters()
 
         #Scrub Data
         inputdata = msg.data
@@ -48,23 +66,21 @@ class VocalizeNode(Node):
 
         if inputdata == self.lastdata:
             self.speech = ""
-        elif inputdata == "ImperialMarch":
-            self.speech = ""
-            bashCommand = "play /home/ubuntu/ros_ws/src/vocalize_ros2/resource/AudioFX/ImperialMarch.mp3"
-            subprocess.run(bashCommand, shell=True)
         else:
             self.speech = inputdata
 
         self.lastdata = inputdata
 
-        bashCommand = "echo " + self.speech + " | espeak-ng -v " + self.voice + " -p " + str(self.pitch) + " -a " + str(self.amplitude) + " -g " + str(self.wordgap) + " -s " + str(self.speed)
-        print(bashCommand)
-        subprocess.run(bashCommand, shell=True, check=True)
+        if self.speech != "":
+            self.get_logger().info('Vocalize: ' + str(self.speech))
+            bashCommand = "echo " + self.speech + " | espeak-ng -v " + self.voice + " -p " + str(self.pitch) + " -a " + str(self.amplitude) + " -g " + str(self.wordgap) + " -s " + str(self.speed)
+            print(bashCommand)
+            subprocess.run(bashCommand, shell=True, check=True)
 
 def main(args=None):
     rclpy.init(args=None)
     vocalize_node = VocalizeNode()
-    print("Starting")
+    print("Starting Vocalize Node")
     rclpy.spin(vocalize_node)
     vocalize_node.destroy_node()
     rclpy.shutdown()
